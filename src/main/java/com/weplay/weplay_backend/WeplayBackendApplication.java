6606347dacda99597f8a3af20be28930969cd1c6
Nagoor Vali Shaik <shaiknagurvali281@gmail.com>
2026-02-22 15:09:41 -0600

    Initial setup: Spring Boot project with User model and MySQL connection

---
+++ src/main/java/com/weplay/weplay_backend/WeplayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.weplay.weplay_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeplayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeplayBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weplay/weplay_backend/WeplayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weplay.weplay_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeplayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
